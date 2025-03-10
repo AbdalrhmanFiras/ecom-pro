b45ad35180a235f7013506154b78e05a0e9c484f
AbdalrhmanFiras <aa3819341@gmail.com>
2025-03-10 21:17:40 +0300

    Add Register & Login

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\API;
+use App\Http\Requests\loginValidate;
+use App\Http\Requests\RegisterValidate;
+use App\Http\Resources\UserResource;
+use GuzzleHttp\Psr7\Response;
+use Hash;
+use Laravel\Sanctum\HasApiTokens;
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Validator;
+
+class UserController extends Controller
+{
+    use HasApiTokens;
+    public function register(RegisterValidate $request)
+    {
+        // the request is coming validated by request form 
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)//dont forget to hashing the password
+        ]);
+        return new UserResource($user);
+    }
+
+    public function login(loginValidate $request)
+    {
+        //the Auth::attempt : check if the user are register or not by (email and password)
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json(['message' => 'invaild email or password'], 401);
+        }
+
+        $user = User::where('email', $request->email)->firstOrFail();//get the user by email
+        $token = $user->createToken('auth-token')->plainTextToken;
+        return response()->json([
+            'User' => new UserResource($user),
+            'token' => $token
+        ], 200);
+    }
+
+
+
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\UserController;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('register', [UserController::class, 'register']);
+Route::post('login', [UserController::class, 'login']);
